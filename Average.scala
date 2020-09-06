506ce3a7701a4665d558dd5e69fa7030732703c7
Amir A. Shabani <amirshabani011@gmail.com>
2020-09-06 23:09:39 +0000

    Ignore anything that is not a number

--- Average.scala
+++ Average.scala
@@ -3,7 +3,7 @@ import scala.io.StdIn._
 object Average {
     def main(args: Array[String]): Unit = {
         print("Enter space-separated numbers: ")
-        val numbers: Array[Double] = readLine().trim.split(" +").map(_.toDouble)
+        val numbers: Array[Double] = readLine().trim.split(" +").map(_.toDoubleOption).flatten
         val average: Double = numbers.sum / numbers.length
         println(f"Average: ${average}%.2f")
     }
