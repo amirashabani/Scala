7bc894351688293d9a34424eaaca4ced28268127
Amir A. Shabani <amirshabani011@gmail.com>
2020-09-06 22:55:56 +0000

    Explicit data types

--- Average.scala
+++ Average.scala
@@ -3,8 +3,8 @@ import scala.io.StdIn._
 object Average {
     def main(args: Array[String]): Unit = {
         println("Enter space-separated numbers:")
-        val numbers = readLine().trim.split(" +").map(_.toDouble)
-        val average = numbers.sum / numbers.length
+        val numbers: Array[Double] = readLine().trim.split(" +").map(_.toDouble)
+        val average: Double = numbers.sum / numbers.length
         println(f"Average: ${average}%.2f")
     }
 }
