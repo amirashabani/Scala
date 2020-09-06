aaba1e59a0fe33eea8eeeea2575519e715dcaac5
Amir A. Shabani <amirshabani011@gmail.com>
2020-09-06 22:58:11 +0000

    Prompt line doesn't need newline

--- Average.scala
+++ Average.scala
@@ -2,7 +2,7 @@ import scala.io.StdIn._
 
 object Average {
     def main(args: Array[String]): Unit = {
-        println("Enter space-separated numbers:")
+        print("Enter space-separated numbers: ")
         val numbers: Array[Double] = readLine().trim.split(" +").map(_.toDouble)
         val average: Double = numbers.sum / numbers.length
         println(f"Average: ${average}%.2f")
